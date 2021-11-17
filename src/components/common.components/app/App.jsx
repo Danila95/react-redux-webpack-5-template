142f8d3bbe907ce4234b371736e38dd34782e8d5
ddb4 <ddb4@tpu.ru>
2021-11-17 11:19:52 +0700

    create component "start-bootstrap"

--- src/components/common.components/app/App.jsx
+++ src/components/common.components/app/App.jsx
@@ -2,6 +2,7 @@ import Header from "../header/header";
 import FirstStartWebpack from "../../first-start-webpack/first-start-webpack";
 import StartReact from "../../start-react/start-react";
 import Recipes from "../../recipes/recipes";
+import StartBootstrap from "../../start-bootstrap/start-bootstrap";
 import Footer from "../footer/footer";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <FirstStartWebpack />
       <StartReact />
       <Recipes />
+      <StartBootstrap />
       <Footer />
     </div>
   );
